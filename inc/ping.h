269530c1f697deefdd30c74a59773189cdd17301
Luma-3 <jb.brs@icloud.com>
2025-10-10 15:39:56 +0200

    chore: adding loop and fix reply parsing

--- inc/ping.h
+++ inc/ping.h
@@ -1,9 +1,13 @@
+#include <stddef.h>
 #ifndef FT__PING
 #define FT__PING 1
 
+#include <netinet/ip_icmp.h>
 #include <sys/types.h>
-void icmp_pack(struct icmphdr* buff);
 
 u_int16_t checksum(u_int16_t* ptr, size_t len);
+
+void            icmp_pack(struct icmphdr* buff, ssize_t seq);
+struct icmphdr* icmp_unpack(void* buff, size_t buff_len, size_t* len);
 
 #endif
--- src/packet.c
+++ src/packet.c
@@ -1,5 +1,7 @@
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+#include <stddef.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -27,11 +29,19 @@ u_int16_t checksum(u_int16_t* ptr, size_t len)
     return ((u_int16_t)~sum);
 }
 
-void icmp_pack(struct icmphdr* buff)
+void icmp_pack(struct icmphdr* buff, ssize_t seq)
 {
     buff->type             = ICMP_ECHO;
     buff->code             = 0;
     buff->checksum         = 0;
     buff->un.echo.id       = htons(getpid() & 0xFFFF);
-    buff->un.echo.sequence = htons(0);
+    buff->un.echo.sequence = htons(seq);
+}
+
+struct icmphdr* icmp_unpack(void* buff, size_t buff_len, size_t* len)
+{
+    struct iphdr* ip_head = (struct iphdr*)buff;
+
+    *len = buff_len - ip_head->ihl * 4;
+    return (struct icmphdr*)(buff + (ip_head->ihl * 4));
 }
